055876c91d012b7e5d86328b5060335fb24972c1
Sergey Voronov <rdkl.hrd@gmail.com>
2014-12-15 15:14:13 +0400

    Fixed main task

--- 3rdparty/main_task/crypt.py
+++ 3rdparty/main_task/crypt.py
@@ -1,24 +1,24 @@
 from sys import argv
 import random
 
-def crypt(output_filename, input_files):
+def crypt(output_filename, input_filenames):
     p = range(26)
     random.shuffle(p)
 
     output = open(output_filename, "w")
 
-    for fname in input_files:
+    for fname in input_filenames:
         f = open(fname, "r")
     
-    for line in f.readlines():
-        for ch in line.strip():
-            try:
-                x = ord(ch) - ord('a')
-                output.write(chr(p[x] + ord('a')))
-            except:
-                print '{} unsupported'.format(ch)
-                output.write('\n')
-    
+        for line in f.readlines():
+            for ch in line.strip():
+                try:
+                    x = ord(ch) - ord('a')
+                    output.write(chr(p[x] + ord('a')))
+                except:
+                    print '{} unsupported'.format(ch)
+                    output.write('\n')
+        
     output.close()
     
     for i in xrange(len(p)):

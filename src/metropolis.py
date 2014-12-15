950504391293ed95c55e60337f69c004eae64fe9
Sergey Voronov <rdkl.hrd@gmail.com>
2014-12-15 18:06:16 +0400

    Added get_start_permutation

---
+++ src/metropolis.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+from estimate_likelihood import estimate_likelihood
+from process_data.get_bigram_frequency import get_bigram_frequency
+
+
+#-----------------------------------------------------------------------------
+def get_start_permutation(text, bigram_distribution):
+    likelihood = 0
+    while likelihood is 0:
+        letters_list = list(string.lowercase)
+        random.shuffle(letters_list)
+        letters = ''.join(letters_list)
+        perm = dict(zip(string.lowercase, letters))
+        likelihood = estimate_likelihood(train_file, bigram_distribution, 
+                                         perm)
+        
+    return perm
+    
+#-----------------------------------------------------------------------------
+if __name__ == "__main__":
+    train_file = "../data/war_and_peace.txt"
+    train_text = open(train_file, "r")
+    train_bigram_distribuion = get_bigram_frequency(train_text) 
+    
+    start_perm = get_start_permutation(train_text, 
+                                       train_bigram_distribuion)
+    

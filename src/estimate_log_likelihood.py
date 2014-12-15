e22ceb6b6003db852ba33adc6a7a50c47eeb1613
Sergey Voronov <rdkl.hrd@gmail.com>
2014-12-16 00:07:34 +0400

    Likelihood -> log_likelihood

--- src/estimate_log_likelihood.py
+++ src/estimate_log_likelihood.py
@@ -1,22 +1,29 @@
+import math
+
 from process_data.get_bigram_frequency import get_bigram_frequency
 
-def estimate_likelihood(text, bigram_cnt, perm):
+
+
+def estimate_log_likelihood(text, bigram_dist, perm, threshold = 10**(-11)):
     '''
     perm is a dictionary: perm[encoded letter] = decoded letter
     '''    
-    likelihood = 1.0
+    log_likelihood = 0.0
 
     for line in text:
         line = line.strip()
         for word in line.split():
             for i in xrange(len(word) - 1):
-                likelihood *= bigram_cnt[perm[word[i]] + perm[word[i + 1]]]
-        
-    return likelihood
+                bigram_probability = bigram_dist[perm[word[i]] + \
+                                                 perm[word[i + 1]]]
+                log_likelihood += math.log(max(bigram_probability, 
+                                                threshold))
+    
+    return log_likelihood
 
 #-----------------------------------------------------------------------------
 if __name__ == "__main__":
-    print estimate_likelihood(["bfbf aa"], 
+    print estimate_log_likelihood(["bfbf aa"], 
                               get_bigram_frequency("../data/test.txt"), 
                               {"a":"f", "b":"a", "f":"b"})
     

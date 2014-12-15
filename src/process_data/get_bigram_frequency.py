0f27eff7e21fd3fcd6da9506736b6dddaaac71fa
Sergey Voronov <rdkl.hrd@gmail.com>
2014-12-15 16:03:35 +0400

    Fixed codestyle and added ability to work with other input files

--- src/process_data/get_bigram_frequency.py
+++ src/process_data/get_bigram_frequency.py
@@ -1,21 +1,21 @@
-from sys import argv
 from collections import Counter
 
 def get_bigram_frequency(input_filename):
     training = open(input_filename, "r")
-    bigram_cnt = Counter()
+    bigram_counter = Counter()
 
     for line in training.readlines():
-        strline = line.strip()
-        for i in xrange(len(strline) - 1):
-            bigram_cnt[strline[i] + strline[i + 1]] += 1.0
+        line = line.strip()
+        for word in line.split():
+            for i in xrange(len(word) - 1):
+                bigram_counter[word[i:i + 2]] += 1.0
         
-    bigram_number = sum(bigram_cnt.values())
-    for key in bigram_cnt.keys():
-        bigram_cnt[key] /= bigram_number
-    return bigram_cnt
+    bigram_number = sum(bigram_counter.values())
+    for key in bigram_counter.keys():
+        bigram_counter[key] /= bigram_number
+    return bigram_counter
 
 #-----------------------------------------------------------------------------
 if __name__ == "__main__":
-    print get_bigram_frequency("test.txt")
+    print get_bigram_frequency("../../data/test.txt")
     

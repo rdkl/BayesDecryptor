9f1d977551f449f87ff3a1cf401c2d7e1763191e
lechanka <avlechenko@gmail.com>
2014-12-17 16:32:56 +0300

    fixes in quality.py file.

--- src/quality.py
+++ src/quality.py
@@ -4,26 +4,31 @@ from time import time
 def quality(x, y, f):
     guessed = 0.0
     total = 0.0
-    checked = {x[i]: False for i in xrange(0, len(x))}
     for i in xrange(0, len(x)):
-        if checked[x[i]] is False and f.get(x[i], None) is not None:
-            total += 1.0
-            if f[x[i]] == y[i]:
-                guessed += 1.0
-            checked[x[i]] = True
+        x_word = x[i]
+        y_word = y[i]
+        for j in xrange(0, len(x_word)):
+            y_decoded = f.get(x_word[j], None)
+            if y_decoded is not None:
+                total += 1.0
+                if y_decoded == y_word[j]:
+                    guessed += 1.0
 
     return guessed / total if total != 0.0 else 1.0
 
 
 if __name__ == "__main__":
-    print quality("bfbf aa",
-                  "abab ff",
+    print quality(["bfbf aa"],
+                  ["abab ff"],
                   {"a": "f", "b": "a", "f": "b"})
-    print quality("bfbf aa",
-                  "bfbf aa",
+    print quality(["bfbf aa"],
+                  ["bfbf aa"],
                   {"a": "f", "b": "a", "f": "b"})
-    print quality("bfbf aa",
-                  "bfbf aa",
+    print quality(["bfbf aa"],
+                  ["bfbf aa"],
+                  {"a": "f", "b": "b", "f": "a"})
+    print quality(["bfbf aa", "bfbf aa"],
+                  ["bfbf aa", "baba ff"],
                   {"a": "f", "b": "b", "f": "a"})
     t = time()
     print quality(
